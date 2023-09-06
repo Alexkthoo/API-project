2fc8288b61e9b32b77a1abc761470a53f04a9c9a
Alex <Alexk.Thoo@gmail.com>
2023-09-06 12:56:22 -0700

    complete create booking side

--- backend/db/migrations/20230906195341-create-booking.js
+++ backend/db/migrations/20230906195341-create-booking.js
@@ -1,37 +1,48 @@
-'use strict';
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Bookings', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    options.tableName = "Bookings";
+    await queryInterface.createTable(
+      "Bookings",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        spotId: {
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+        },
+        startDate: {
+          type: Sequelize.DATEONLY,
+        },
+        endDate: {
+          type: Sequelize.DATEONLY,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
       },
-      spotId: {
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      startDate: {
-        type: Sequelize.DATEONLY
-      },
-      endDate: {
-        type: Sequelize.DATEONLY
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
-  }
+    options.tableName = "Bookings";
+    await queryInterface.dropTable("Bookings");
+  },
 };
