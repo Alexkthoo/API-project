5280ef54c443c03763a86adef827304c4a3db771
Alex <Alexk.Thoo@gmail.com>
2023-09-05 20:57:23 -0700

    complete log in

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -11,10 +11,10 @@ const validateLogin = [
   check("credential")
     .exists({ checkFalsy: true })
     .notEmpty()
-    .withMessage("Please provide a valid email or username."),
+    .withMessage("Email or username is required"),
   check("password")
     .exists({ checkFalsy: true })
-    .withMessage("Please provide a password."),
+    .withMessage("Password is required"),
   handleValidationErrors,
 ];
 
@@ -34,15 +34,14 @@ router.post("/", validateLogin, async (req, res, next) => {
   });
 
   if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-    const err = new Error("Login failed");
-    err.status = 401;
-    err.title = "Login failed";
-    err.errors = { credential: "The provided credentials were invalid." };
-    return next(err);
+    // Return the error response directly to the client
+    return res.status(401).json({ message: "Invalid credentials" });
   }
 
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };
