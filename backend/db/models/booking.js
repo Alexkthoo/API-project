886df65260627edbddb9ccb651b2585da1a46721
Alex <Alexk.Thoo@gmail.com>
2023-09-07 00:03:16 -0700

    complete booking seed

--- backend/db/models/booking.js
+++ backend/db/models/booking.js
@@ -22,15 +22,19 @@ module.exports = (sequelize, DataTypes) => {
     {
       spotId: {
         type: DataTypes.INTEGER,
+        allowNull: false,
       },
       userId: {
         type: DataTypes.INTEGER,
+        allowNull: false,
       },
       startDate: {
         type: DataTypes.DATEONLY,
+        allowNull: false,
       },
       endDate: {
         type: DataTypes.DATEONLY,
+        allowNull: false,
       },
     },
     {
