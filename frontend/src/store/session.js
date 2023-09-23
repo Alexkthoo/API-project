2cebaf67039c849938f84e1d0b169204288ead9d
Alex <Alexk.Thoo@gmail.com>
2023-09-23 01:27:40 -0700

    complete phase 2

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -41,6 +41,24 @@ export const restoreUser = () => async (dispatch) => {
   return response;
 };
 
+// sign up
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 // REDUCER FUNCTION
 const initialState = { user: null };
 
