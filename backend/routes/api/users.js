ec5daeae003196ac1bfbcf5d28cc66ab50127776
Alex <Alexk.Thoo@gmail.com>
2023-10-27 17:17:38 -0700

    finish sign-up css

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -10,7 +10,7 @@ const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage("Invalid email"),
+    .withMessage("Invalid email. Please Enter a valid Email."),
   check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -53,74 +53,100 @@ function SignupFormModal() {
     password.length < 6;
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </label>
-        {errors.username && <p>{errors.username}</p>}
-        <label>
-          First Name
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.firstName && <p>{errors.firstName}</p>}
-        <label>
-          Last Name
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required
-          />
-        </label>
-        {errors.lastName && <p>{errors.lastName}</p>}
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
-          <input
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            required
-          />
-        </label>
-        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit" disabled={disableSignUp}>
-          Sign Up
-        </button>
-      </form>
-    </>
+    <div className="sign-up-container">
+      <div className="sign-up-sign">
+        <h1>Sign Up</h1>
+      </div>
+      <div className="sign-up-form-box">
+        <form onSubmit={handleSubmit} className="sign-up-form">
+          <div>
+            <label>
+              Email
+              <input
+                placeholder="ex. WorldofWarcraft@WoW.com"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </label>
+          </div>
+          {errors.email && <p>{errors.email}</p>}
+          <div>
+            <label>
+              Username
+              <input
+                placeholder="Username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+            </label>
+            {errors.username && <p>{errors.username}</p>}
+          </div>
+          <div>
+            <label>
+              First Name
+              <input
+                placeholder="First Name"
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              />
+            </label>
+            {errors.firstName && <p>{errors.firstName}</p>}
+          </div>
+          <div>
+            <label>
+              Last Name
+              <input
+                placeholder="Last Name"
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              />
+            </label>
+            {errors.lastName && <p>{errors.lastName}</p>}
+          </div>
+          <div>
+            <label>
+              Password
+              <input
+                placeholder="Password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            {errors.password && <p>{errors.password}</p>}
+          </div>
+          <div>
+            <label>
+              Confirm Password
+              <input
+                placeholder="Confirm Password"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </label>
+            {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+          </div>
+          <button
+            type="submit"
+            disabled={disableSignUp}
+            className="sign-up-submit-button"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
